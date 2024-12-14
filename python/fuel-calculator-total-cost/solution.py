ac051e0ba471977d647c5ccca744690ef707415e
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-12-14 15:15:31 +0100

    feat(Solución): :sparkles: Añadir Fuel Calculator: Total Cost

---
+++ python/fuel-calculator-total-cost/solution.py
@@ -0,0 +1,13 @@
+discount = 0
+discount_price = 0
+def fuel_price(litres, price_per_litre):
+    if litres % 2 == 0:
+        discount = litres / 2 * 5
+    else:
+        discount = (litres - 1) / 2 * 5
+
+    if discount >= 25: discount = 25
+
+    discount_price = round(litres * (price_per_litre - discount / 100) , 2)
+
+    return discount_price
